cceeb66fe59fc4cc267e36f4d646afaf86e56caa
Luca Spindler <luca.spindler@mni.thm.de>
2014-11-28 18:04:46 +0100

    erste version

--- index.php
+++ index.php
@@ -1,4 +1,69 @@
-<?php
-	echo "Hello World";
-	phpinfo();
-?>
+
+	body{
+ background-color: #b7d1c4 ;  
+}
+div{
+    border-radius:5px;
+    border-collapse: collapse;
+}
+ul{
+   list-style-type: none;
+   position: fixed;
+   margin: -10px;
+}
+.oben{
+    color: #ffffff;
+    top:15px;
+    display: inline;
+	border: 1px solid #000000;
+	padding: 5px;
+	border-radius: 35px 5px;
+	background-color:blue;
+	}
+
+#navbar {
+	position: fixed;
+	top:10px;
+	left:50%;
+	border-collapse: collapse;
+	margin-left:-254px;
+}
+#header{
+    top:10px;
+    border-radius:15%;
+	height: 60px;
+	width: 50%;
+	background-color: #668284;
+	position:fixed;	
+	
+	left:25%;
+	z-index:1;
+}
+
+.left{
+    position: relative;
+	float: left;
+	margin-top: 50px;
+	width: 35%;
+	height: 400px;
+	background-color: #B9D7D9;
+	margin-bottom: 10px;
+}
+
+.right{
+    height: 50px;
+	width: 55%;
+	position:relative;
+	float:right;
+	margin-left: 5px;
+	margin-right: 5px;
+	margin-top: 50px;
+}
+
+#footer{
+	height: 20px;
+	width: 95%;
+	background-color: #668284;
+	clear: both;
+}
+
